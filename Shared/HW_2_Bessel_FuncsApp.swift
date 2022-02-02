6ac7704345fd1ece018ae072d365fda4dbd02863
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-02 17:15:03 -0600

    Initial Commit

---
+++ Shared/HW_2_Bessel_FuncsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_2_Bessel_FuncsApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HW_2_Bessel_FuncsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
